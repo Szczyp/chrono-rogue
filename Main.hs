e7109cabb825f9183423249b5b82653ba354991f
Szczyp <qb@szczyp.com>
2013-06-10 22:50:42 +0200

    More fun with typeclasses

--- Main.hs
+++ Main.hs
@@ -1,12 +1,9 @@
 module Main where
 
 import Prelude hiding (lookup)
-import Control.Applicative ((<$>))
-import Control.Arrow ((&&&))
 import Control.Monad
 import qualified Data.Map as M
 import Data.Maybe
-import Data.Monoid
 
 type Coord = (Int, Int)
 
@@ -15,47 +12,62 @@ data Level = Level { levelWalls  :: [Wall]
                    , levelItems  :: [Item] }
 
 
+type RenderInfo = (Coord, Char)
+
+class (Position renderable, Sigil renderable) => Renderable renderable where
+    render :: renderable -> RenderInfo
+    render renderable = (position renderable, sigil renderable)
+
+
 class Position p where
-    getPosition :: p -> Coord
+    position :: p -> Coord
 
 class Sigil s where
-    getSigil :: s -> Char
+    sigil :: s -> Char
 
 
 data Hero = Hero { heroPosition :: Coord }
 
 instance Position Hero where
-    getPosition = heroPosition
+    position = heroPosition
 
 instance Sigil Hero where
-    getSigil = const '@'
+    sigil = const '@'
+
+instance Renderable Hero
 
 
 data Wall = Wall { wallPosition :: Coord }
 
 instance Position Wall where
-    getPosition = wallPosition
+    position = wallPosition
 
 instance Sigil Wall where
-    getSigil = const '#'
+    sigil = const '#'
+
+instance Renderable Wall
 
 
 data Item = Item { itemPosition :: Coord
                  , itemSigil    :: Char }
 
 instance Position Item where
-    getPosition = itemPosition
+    position = itemPosition
 
 instance Sigil Item where
-    getSigil = itemSigil
+    sigil = itemSigil
+
+instance Renderable Item
 
 
 defaultHero :: Hero
 defaultHero = Hero { heroPosition = (2, 2) }
 
+
 defaultItem :: Item
 defaultItem = Item { itemPosition = (7, 8)
                    , itemSigil = '$' }
+
 
 squareWall :: Int -> [Wall]
 squareWall size = do
@@ -64,20 +76,20 @@ squareWall size = do
     guard $ x == 1 || x == size || y == 1 || y == size
     return Wall { wallPosition = (x, y) }
 
+
 defaultLevel :: Level
 defaultLevel = Level { levelHeroes = [defaultHero]
                      , levelWalls = squareWall 10
                      , levelItems = [defaultItem] }
 
+
 drawLevel :: Level -> String
-drawLevel level = unlines . map row $ [1 .. 10]
-  where row y = map (sigil y) [1 .. 10]
-        sigil y x = fromMaybe ' ' . getFirst . mconcat . map (First . ($ (x, y))) $ lookups
-        lookups = [ lookup levelWalls
-                  , lookup levelHeroes
-                  , lookup levelItems ]
-        lookup select coord = getSigil <$> M.lookup coord (toMap . select $ level)
-        toMap xs = M.fromList $ map (getPosition &&& id) xs
+drawLevel level = unlines . map makeRow $ [1 .. 10]
+  where makeRow y = map (sigilOrDot y) [1 .. 10]
+        sigilOrDot y x = fromMaybe '.' . M.lookup (x, y) $ coordMap
+        coordMap = M.unions [toMap levelHeroes, toMap levelWalls, toMap levelItems]
+        toMap select = M.fromList . map render . select $ level
+
 
 main :: IO ()
 main = putStr $ drawLevel defaultLevel
